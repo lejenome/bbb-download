9d56e9cc178defb08179ca3fa7069d69b4ccd6ad
Moez Bouhlel <lejenome@tik.tn>
2020-05-17 13:03:16 +0100

    add download ready url notification

--- src/download_control.rb
+++ src/download_control.rb
@@ -1,36 +1,17 @@
 #!/usr/bin/ruby
 # encoding: UTF-8
 
-#
-# BigBlueButton open source conferencing system - http://www.bigbluebutton.org/
-#
-# Copyright (c) 2012 BigBlueButton Inc. and by respective authors (see below).
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation; either version 3.0 of the License, or (at your option)
-# any later version.
-#
-# BigBlueButton is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Lesser General Public License along
-# with BigBlueButton; if not, see <http://www.gnu.org/licenses/>.
-#
-
 require "trollop"
 require File.expand_path('../../../lib/recordandplayback', __FILE__)
+
+logger = Logger.new("/var/log/bigbluebutton/post_publish.log", 'weekly' )
+logger.level = Logger::INFO
+BigBlueButton.logger = logger
 
 opts = Trollop::options do
   opt :meeting_id, "Meeting id to archive", :type => String
 end
 meeting_id = opts[:meeting_id]
-
-logger = Logger.new("/var/log/bigbluebutton/post_publish.log", 'weekly' )
-logger.level = Logger::INFO
-BigBlueButton.logger = logger
 
 published_files = "/var/bigbluebutton/published/presentation/#{meeting_id}"
 meeting_metadata = BigBlueButton::Events.get_meeting_metadata("/var/bigbluebutton/recording/raw/#{meeting_id}/events.xml")
@@ -38,9 +19,51 @@ meeting_metadata = BigBlueButton::Events.get_meeting_metadata("/var/bigbluebutto
 #
 # This runs the download script
 #
-download_status = system("/usr/bin/python3 /usr/local/bigbluebutton/core/scripts/post_publish/download.py #{meeting_id}")
+BigBlueButton.logger.info("Recording download video generation for [#{meeting_id}] starts")
 
+begin
+  callback_url = meeting_metadata.key?("bbb-download-ready-url") ? meeting_metadata["bbb-download-ready-url"].value : nil
 
+  unless callback_url.nil?
+    download_status = system("/usr/bin/python3 /usr/local/bigbluebutton/core/scripts/post_publish/download.py #{meeting_id}")
+  end
 
+  unless callback_url.nil?
+    props = JavaProperties::Properties.new("/usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties")
+    secret = props[:securitySalt]
+    external_meeting_id = meeting_metadata["meetingId"].value
+
+    payload = { meeting_id: external_meeting_id, record_id: meeting_id }
+    payload_encoded = JWT.encode(payload, secret)
+
+    uri = URI.parse(callback_url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = (uri.scheme == 'https')
+
+    BigBlueButton.logger.info("Sending request to #{uri.scheme}://#{uri.host}#{uri.request_uri}")
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.set_form_data({ signed_parameters: payload_encoded })
+
+    response = http.request(request)
+    code = response.code.to_i
+
+    if code == 410
+      BigBlueButton.logger.info("Notified for deleted meeting: #{meeting_id}")
+      # TODO: should we automatically delete the recording here?
+    elsif code == 404
+      BigBlueButton.logger.info("404 error when notifying for recording: #{meeting_id}, ignoring")
+    elsif code < 200 || code >= 300
+      BigBlueButton.logger.info("Callback HTTP request failed: #{response.code} #{response.message} (code #{code})")
+    else
+      BigBlueButton.logger.info("Recording notifier successful: #{meeting_id} (code #{code})")
+    end
+  end
+
+rescue => e
+  BigBlueButton.logger.info("Rescued")
+  BigBlueButton.logger.info(e.to_s)
+end
+
+BigBlueButton.logger.info("Download video ready notify ends")
 
 exit 0
